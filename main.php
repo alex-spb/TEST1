2ab1c28a58b367defeba8e874688700e7fc9caf0
pavlov <pavlov@waveaccess.ru>
2016-12-20 10:26:01 +0300

    commit 2016-12-20 - 001

---
+++ main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Info page</title>
+</head>
+<body>
+info 2
+<?
+ $j = 2 + 12313;
+
+
+      echo 'test2' . $j;
+
+    ?>
+
+
+</body>
+</html>
